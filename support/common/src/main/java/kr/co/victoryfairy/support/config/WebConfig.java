b17131ffe448a778d6fd59c999f57ed1f26f6650
dfdf0202 <dfdf1245@daum.net>
2025-04-11 20:38:51 +0900

    fix: cors 처리

--- support/common/src/main/java/kr/co/victoryfairy/support/config/WebConfig.java
+++ support/common/src/main/java/kr/co/victoryfairy/support/config/WebConfig.java
@@ -4,6 +4,7 @@ import kr.co.victoryfairy.support.interceptor.CurlCommandErrorInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +16,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final CurlCommandErrorInterceptor curlCommandErrorInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 혹은 "/**"
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
